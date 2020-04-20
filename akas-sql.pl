2316845d94926dc7b3ea8ebb563397085b1b257d
MrTamfanX <50101968+MrTamfanX@users.noreply.github.com>
2020-04-20 12:43:33 +0700

    Add files via upload

---
+++ akas-sql.pl
@@ -0,0 +1,316 @@
+#!usr/bin/tamfan/perl
+#hayo mau recode ya ? v: emang lu ngerti bahasa perl ? kiddies
+#ga seneng ? gas chat wa : 0859-6634-6141 & 0857-8041-1404
+#kalo lu recode pasti bakalan syntax v: percaya dah v: awokwokwok v: kalo bisa monggo recode v:
+#subscribe tutorial mrtamfanx - follow instagram mrtamfanx cyber team
+use LWP::UserAgent; 
+use Term::ANSIColor;
+use LWP::Simple;
+use HTTP::Request;
+
+$waktukas = localtime();
+system('xdg-open https://www.youtube.com/channel/UCgxatcJPFCr2vWDj8e0rhsQ');
+mrtamfanx();
+prakasapilih();
+sub mrtamfanx {
+    system('clear');
+    print color('bold yellow'),"╭──────────╮ ";
+    print color('bold green'),"┏━━━━━┓ ┏━━━━━┓ ┏━━━━━┓ ┳   ┳ ┏━━━━━┓ ┏━━━━━┓\n";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"● ";
+    print color('bold green'),"---";
+    print color('bold cyan'),"⊙°° ";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"███";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃     ┻\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃";
+    print color('bold white'),"█";
+    print color('bold green'),"┏━┛ ┃";
+    print color('bold white'),"█████";
+    print color('bold green'),"┃ ┃\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┣━━━━━┛ ┣━━┳━━┛ ┣━━━━━┫ ┣━┫   ┣━━━━━┫ ┗━━━━━┓\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┃       ┃  ┗━┓  ┃┏━━━┓┃ ┃";
+    print color('bold white'),"█";
+    print color('bold green'),"┗━┓ ┃┏━━━┓┃       ┃\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┃       ┃    ┃  ┣┛   ┗┫ ┃";
+    print color('bold white'),"███";
+    print color('bold green'),"┃ ┣┛   ┗┫ ┳     ┃\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold green'),"┻       ┻    ┻  ┻     ┻ ┻   ┻ ┻     ┻ ┗━━━━━┛\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"┃ ";
+    print color('bold green'),"•";
+    print color('bold cyan'),"Author ";
+    print color('bold green'),": ";
+    print color('bold white'),"MrTamfanX ";
+    print color('bold blue'),"Facebook ";
+    print color('bold green'),": ";
+    print color('bold white'),"Prakasa Jr";
+    print color('bold green'),"• ";
+    print color('bold cyan'),"┃\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"┃ ";
+    print color('bold green'),"•";
+    print color('bold red'),"YouTube ";
+    print color('bold green'),": ";
+    print color('bold white'),"Tutorial MrTamfanX";
+    print color('bold green')," - ";
+    print color('bold red'),"SUBSCRIBE";
+    print color('bold green'),"• ";
+    print color('bold cyan'),"┃\n";
+    print color('bold yellow'),"┃";
+    print color('bold white'),"██████████";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"┃ ";
+    print color('bold green'),"•";
+    print color('bold yellow'),"Instagram ";
+    print color('bold green'),": ";
+    print color('bold white'),"MrTamfanX Cyber Team";
+    print color('bold green')," - ";
+    print color('bold blue'),"FOLOW";
+    print color('bold green'),"• ";
+    print color('bold cyan'),"┃\n";
+    print color('bold yellow'),"┃   ";
+    print color('bold red')," ⭕    ";
+    print color('bold yellow'),"┃ ";
+    print color('bold cyan'),"┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n";
+    print color('bold yellow'),"╰──────────╯\n";
+    print color('bold white'),"╾─────────────────────────────────────────────────────────╼\n";
+    print color('bold white'),"[";
+    print color('bold red'),"•";
+    print color('bold white'),"] ";
+    print color('bold yellow'),"Tools Ini Untuk Mencari Web Vuln Dan Melakukan Inject";
+    print color('bold red'),"..\n\n";
+}
+
+sub prakasapilih {
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"]";
+     print color('bold yellow')," $waktukas\n";
+     print color('bold white'),"[";
+     print color('bold green'),"1";
+     print color('bold white'),"] ";
+     print color('bold cyan'),"Cari Dork Vuln SQL";
+     print color('bold red')," Tidak Support Di Android\n";
+     print color('bold white'),"[";
+     print color('bold green'),"2";
+     print color('bold white'),"] ";
+     print color('bold cyan'),"Melakukan SQL INJECTION";
+     print color('bold red'),"\n";
+     print color('bold white'),"[";
+     print color('bold green'),"3";
+     print color('bold white'),"]";
+     print color('bold cyan')," Informasi\n";
+     print color('bold white'),"[";
+     print color('bold green'),"4";
+     print color('bold white'),"]";
+     print color('bold cyan')," Visit BLOG\n";
+     print color('bold white'),"[";
+     print color('bold green'),"5";
+     print color('bold white'),"]";
+     print color('bold cyan')," Keluar\n\n";
+     print color('bold white'),"[";
+     print color('bold yellow'),"•";
+     print color('bold white'),"]";
+     print color('bold cyan'),"Masukan Nomer Nya";
+     print color('bold green')," : ";
+     print color('bold yellow'),"";
+
+    chomp($prakasapilih=<STDIN>);
+
+     if($prakasapilih eq '1'){
+     mrtamfanx();
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"]";
+     print color('bold yellow')," $waktukas\n";
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"] ";
+     print color('bold yellow'),"Contoh Penggunaan ? subscribe.php?id=36 \n";
+     print color('bold white'),"[";
+     print color('bold green'),"•";
+     print color('bold white'),"] ";
+     print color('bold green'),"Masukan Dorknya Mas : ";
+     print color('bold white'),"";
+     $scankas=<STDIN>;
+     chomp($scankas=<STDIN>);
+     print color('bold white'),"[";
+     print color('bold green'),"WAIT";
+     print color('bold white'),"]";
+     print color('bold cyan')," Sedangan Melakukan Scan ";
+     print color('bold green'),": ";
+     print color('bold yellow')," $scankas";
+     print color('bold white'),"";
+     for ($pademangan = 0; $pademangan < 1000; $pademangan += 10) { 
+     $kas = LWP::UserAgent->new(agent => 'Mozilla/5.0 (X11; U; Linux; i686; en-US; rv:1.6) Gecko Debian/1.6-7'); 
+     $kas->timeout(30); 
+     $kas->env_proxy; 
+     $akas = $kas->get('http://www.bing.com/search?q=' . $scankas . '&first=' . $pademangan . '&FORM=PERE')->content; 
+     $skip = index($akas, 'sb_pagN'); 
+     while (1) { 
+     $ancol = index($akas, '<h4><a href="'); 
+     if ($ancol == -1) { 
+     last; 
+     } 
+     $akas = substr($akas, $ancol + 13); 
+     $subrek = substr($akas, 0, index($akas, '"'));
+     $subrek = open (txt,"akaslist.txt"); 
+     print txt $subrek,"\n"; 
+     } 
+     if ($skip == -1) { 
+     last; 
+     } 
+     }
+     system("akaslist.txt");
+     exit;
+    }if($prakasapilih eq '2'){
+     mrtamfanx();
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"]";
+     print color('bold yellow')," $waktukas\n";
+     print color('bold white'),"[";
+     print color('bold green'),"file";
+     print color('bold white'),"]";
+     print color('bold cyan')," Masukan File Listnya ";
+     print color('bold green'),": ";
+     print color('bold white'),"";
+     $site=<STDIN>;
+     print color('bold white'),"[";
+     print color('bold green'),"WAIT";
+     print color('bold white'),"]";
+     print color('bold cyan')," Sedangan Melakukan Scan ";
+     print color('bold green'),": ";
+     print color('bold yellow')," $site";
+     print color('bold white'),"";
+     sleep(10);
+     if (!$ARGV[0]) {
+     }
+     open( SITE, "< $ARGV[0]" ) or die( "Sistem Kami Tidak Bisa Membuka File Anda : $site");
+     our @array = <SITE>;
+     $akasjrr = $array;
+
+     for ($i = 0; $i <= $akasjrr; $i++) {
+
+     $tamfanxperl = "$array[$i]";
+
+     if($tamfanxperl !~ /http:\/\//) { $tamfanxperl = "http://$tamfanxperl"; }
+
+     $site = "$tamfanxperl";
+
+     my $req=HTTP::Request->new(GET=>$site);
+     my $ua=LWP::UserAgent->new();
+     $ua->timeout(15);
+     my $respontkas=$ua->request($req);
+
+     if($respontkas->content =~ /You have an error in your SQL syntax/ ||
+     $respontkas->content =~ /MySQL server version/ ||
+     $respontkas->content =~ /Syntax error converting the nvarchar value/ ||
+     $respontkas->content =~ /Unclosed/ ||
+     $respontkas->content =~ /SQL Server error/ ||
+     $respontkas->content =~ /JET/) {
+     print "\n \t $tamfanxperl \n";
+
+     open (NOTEPAD, "akaslist.txt");
+     print NOTEPAD "$site\n";
+     close(NOTEPAD);
+
+     }
+     }
+     print color('bold white'),"[";
+     print color('bold green'),"✓";
+     print color('bold white'),"]";
+     print color('bold yellow')," Selesai Mas";
+     print color('bold red'),"...";
+     print color('bold white'),"\n";
+    }if($prakasapilih eq '3'){
+     mrtamfanx();
+     mrtamfanx();
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"]";
+     print color('bold yellow')," $waktukas\n";
+     print color('bold white'),"[";
+     print color('bold yellow'),"INFO";
+     print color('bold white'),"]";
+     print color('bold green')," Script Atau Tools Ini Di Buat Oleh Prakasa Jr64 Atau MrTamfanX Untuk Melakukan Mencari Sebuah Dork Vuln Dan Melakukan Injection... Script Atau Tools Ini Tidak Boleh Di Recode... Karena Apa ? Saya Cape Membuatnya. Jika Ada Yang Merecode Maka Dia Adalah Lammer...\n\n";
+     print color('bold white'),"[";
+     print color('bold yellow'),"CARA";
+     print color('bold white'),"]";
+     print color('bold green')," Cara Penggunaan-nya Cukup Simple, Anda Bisa Memasukan Dork Dan Anda Juga Bisa Membuat File List Terlebih Dahulu Dengan Nama File akaslist.txt Untuk Melakukan Injection...\n";
+     print color('bold white'),"";
+    }if($prakasapilih eq '4'){
+     system('xdg-open http://blog.tutorialmrtamfanx.xyz');
+     system('perl akas-sql.pl');
+    }if($prakasapilih eq '5'){
+     mrtamfanx();
+     menukaskas();
+    }
+}
+sub menukaskas {
+     print color('bold white'),"[";
+     print color('bold red'),"•";
+     print color('bold white'),"]";
+     print color('bold yellow')," $waktukas\n";
+     print color('bold white'),"[";
+     print color('bold green'),"?";
+     print color('bold white'),"]";
+     print color('bold green')," Apakah Anda Yakin Mau Keluar ? IYA/TIDAK\n";
+     print color('bold white'),"[";
+     print color('bold green'),"•";
+     print color('bold white'),"]";
+     print color('bold cyan')," Jawaban Anda ";
+     print color('bold green'),": ";
+     print color('bold white'),"";
+     $tamfanakas=<STDIN>;
+     chomp($tamfanakas=<STDIN>);
+     if($tamfankas eq 'IYA'){
+     system('xdg-open https://www.youtube.com/channel/UCgxatcJPFCr2vWDj8e0rhsQ');
+    }if($tamfankas eq 'TIDAK'){
+     system('perl akas-sql.pl');
+    }if($tamfankas eq 'iya'){
+     system('xdg-open https://www.youtube.com/channel/UCgxatcJPFCr2vWDj8e0rhsQ');
+    }if($tamfankas eq 'tidak'){
+     system('perl akas-sql.pl');
+    }
+}
+
